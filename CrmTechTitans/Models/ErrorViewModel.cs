17983386e2eec2cea4280f95d7d42820ebdcdeb3
Begoude Rikong Austeen <begoudeausteen30@gmail.com>
2025-01-22 11:07:18 -0500

    Delete CrmTechTitans directory

--- CrmTechTitans/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CrmTechTitans.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
